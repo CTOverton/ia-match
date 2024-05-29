b25a33b0cbaa1b677ac0a3ab1ca3f20d58325ccc
ctoverton <christianoverton@ctoverton.com>
2024-05-29 16:50:48 -0400

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
-
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <div className={'absolute h-full w-full'}>
+        <div className={'flex h-full w-full items-center justify-center'}>
+          <div
+            className={
+              'rounded-md bg-gray-400/10 px-4 py-2 ring-1 ring-inset ring-gray-400/20'
+            }
+          >
+            <h2>Home</h2>
+          </div>
+        </div>
+      </div>
     </main>
-  );
+  )
 }
